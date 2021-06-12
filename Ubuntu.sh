c01813812f798ee1ce6b153b2d69e7b3f68b0bf6
fa.benavideso@uniandes.edu.co <fabenavideso@gmail.com>
2021-06-11 22:02:52 -0500

    [added] ubuntu analisys

---
+++ Ubuntu.sh
@@ -0,0 +1,14 @@
+# Containers are not meant to host an operating system. Containers are meant to run a specific task or process such as to host an instance of a web server
+
+# Run a container with ubuntu will exit immediately
+docker run ubuntu
+docker ps   # no container running
+docker ps -a    # Exited (0) About a minute ago
+
+# A container only lives as long as the process inside it
+# EXECUTE A COMMAND INSIDE A CONTAINER
+docker run ubuntu sleep 100
+docker ps -a    # container will run because there is a task running
+docker exec <container_name> cat /etc/hosts # you can execute something inside container
+
+
